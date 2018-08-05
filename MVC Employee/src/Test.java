14bea99ffa4332a60a765a506c9de3e307843bf3
PoojaKarnik195 <41577579+PoojaKarnik195@users.noreply.github.com>
2018-08-06 00:09:55 +0530

    Add files via upload

---
+++ MVC Employee/src/Test.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import com.cg.ui.EmployeeUI;
+
+/**
+ * @author Pooja Karnik
+ * 
+ *         contains the main method to start the Employee Application
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		new EmployeeUI().start();
+	}
+}
